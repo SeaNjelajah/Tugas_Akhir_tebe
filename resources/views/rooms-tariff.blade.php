1a6c420f0fe21eff34b1588828a9a35474d02e33
SeaNjelajah <hani12alhayyan@gmail.com>
2022-03-29 14:16:32 +0700

    first commit

---
+++ resources/views/rooms-tariff.blade.php
@@ -0,0 +1,28 @@
+@include('layout.header')
+
+<div class="container">
+
+<h2>Kamar dan Harga</h2>
+
+
+<!-- form -->
+
+<div class="rowr" style="box-sizing: border-box">
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/8.jpg" class="img-responsive"><div class="info"><h3>Deluxe Room</h3><p> Deluxe Room tersedia dalam dua pilihan,tempat tidur double bed dan twin single bed. Di lengkapi dengan kamar mandi pribadi dengan shower berdiri.</p><a href="room-details.php" class="btn btn-default">Rp.150.000,00</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/9.jpg" class="img-responsive"><div class="info"><h3>Executive Deluxe Room</h3><p>Executive Deluxe Room tersedia dalam tempat tidur double dan twin, lengkap dengan kamar mandi pribadi yang di lengkapi dengan bathub.</p><a href="room-details.php" class="btn btn-default">Rp.165.000,00</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/10.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Bantalan bergaya ini menggabungkan dekorasi minimalis dan kenyamanan modern hotel Urban di Surabaya, yang dirancang untuk istirahat dengan keraguan tinggi.</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/11.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Nikmati matahari dan nikmati pemandangan indah Surabaya dari balkon pribadi, atau hanya meringkuk di daybed Anda tidak pernah santai seperti apa yang manfaat ruangan ini bagi Anda.</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/9.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Ruangan ini adalah segalanya liburan dengan keluarga atau teman-teman harus - pribadi, damai dan senyaman mungkin. Nikmati meja makan yang unik, untuk sekadar memiliki roti istirahat.</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/8.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p>Masing-masing kamar yang luas ini dirancang untuk kenyamanan yang tak tertandingi, dilengkapi dengan fasilitas dan teknologi modern untuk memanjakan dan menghibur.</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/10.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/11.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/9.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/8.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/11.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>
+  <div class="col-sm-6 wowload fadeInUp"><div class="rooms"><img src="images/photos/10.jpg" class="img-responsive"><div class="info"><h3>Luxirious Suites</h3><p> Missed lovers way one vanity wishes nay but. Use shy seemed within twenty wished old few regret passed. Absolute one hastened mrs any sensible</p><a href="room-details.php" class="btn btn-default">Check Details</a></div></div></div>  
+</div>
+
+
+
+</div>
+@include('layout.header')
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('layout.index');
+});
+
+Route::get('/kamar', function(){
+    return view('rooms-tariff');
+})->name('kamar');
+
+Route::get('/pengantar', function(){
+    return view('introduction');
+})->name('kamar');
+
+Route::get('/kamar', function(){
+    return view('rooms-tariff');
+})->name('kamar');
+
+Route::get('/kamar', function(){
+    return view('rooms-tariff');
+})->name('kamar');
+
+Route::get('/kamar', function(){
+    return view('rooms-tariff');
+})->name('kamar');
