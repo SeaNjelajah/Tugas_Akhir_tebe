6912e795d3e7825fb4885649b24247f0ba390e46
SeaNjelajah <hani12alhayyan@gmail.com>
2022-03-29 18:37:06 +0700

    Menyiapkan AdminLte versi 3.2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::prefix('admin')->name('admin.')->group( function () {
+    Route::view('/' , 'admin.dashboard.main')->name('dashboard');
+});
+
 Route::get('/', function () {
     return view('home');
 })->name('home');
